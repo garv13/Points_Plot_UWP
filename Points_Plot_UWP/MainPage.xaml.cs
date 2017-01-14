f46fde995cca0865c1ce6053561e3a2887774673
garv13 <garvjasuja@outlook.com>
2017-01-14 16:38:34 +0530

    lets start the work

--- Points_Plot_UWP/MainPage.xaml.cs
+++ Points_Plot_UWP/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +23,39 @@ namespace Points_Plot_UWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        float prev_X = 100, prev_Y = 100;
         public MainPage()
         {
             this.InitializeComponent();
+        }
+        int NumberOfPositions = 90;
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+                double xStep = 1.0 / NumberOfPositions;
+                double[] yValues = new double[NumberOfPositions + 1];
+                double[] xValues = new double[NumberOfPositions + 1];
+                for (int i = 0; i < NumberOfPositions + 1; i++)
+                {
+                    xValues[i] = (i * xStep) ;
+                    yValues[i] = (Math.Sin(xValues[i] * 2 * Math.PI)) ; // Since you want 2*PI to be @ 1
+                }
+            for (int i = 0; i < NumberOfPositions + 1; i++)
+            {
+                draw_Line((float)xValues[i],(float)yValues[i]);
+            }
+        }
+
+        public void draw_Line(float x,float y)
+        {
+            var line1 = new Line();
+            line1.Stroke = new SolidColorBrush(Windows.UI.Colors.Red);
+            line1.X1 = prev_X;
+            line1.Y1 = prev_Y;
+            line1.X2 = x;
+            line1.Y2 = y;
+            prev_X = x;
+            prev_Y = y;
+            layoutRoot.Children.Add(line1);
         }
     }
 }
---
+++ Points_Plot_UWP/MatrixClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Points_Plot_UWP
+{
+    class MatrixClass
+    {
+        double q1, q2, q3, q0;
+        double sq1, sq2, sq3, sq0;
+        public int ch0, ch1, ch2, ch3; //compared values
+        public double[,] rotation_matrix = new double [3,3];
+
+        public MatrixClass(double x, double y, double z, double q_0)
+        {
+            q1 = x;
+            q2 = y;
+            q3 = z;
+            q0 = q_0;
+        }
+
+        public void convert()
+        {
+            rotation_matrix[0, 0] = (q0 * q0) + (q1 * q1) - (q2 * q2) - (q3 * q3);
+            rotation_matrix[0, 1] = 2 * (-q0 * q3 + q1 * q2);
+            rotation_matrix[0, 2] = 2 * (q0 * q2 + q1 * q3);
+            rotation_matrix[1, 0] = 2 * (q0 * q3 + q2 * q1);
+            rotation_matrix[1, 1] = (q0 * q0) - (q1 * q1) + (q2 * q2) - (q3 * q3);
+            rotation_matrix[1, 2] = 2 * (-q0 * q1 + q2 * q3);
+            rotation_matrix[2, 0] = 2 * (-q0 * q2 + q1 * q3);
+            rotation_matrix[2, 1] = 2 * (q0 * q1 + q2 * q3);
+            rotation_matrix[2, 2] = (q0 * q0) - (q1 * q1) - (q2 * q2) + (q3 * q3);
+        }
+
+        public void reverse()
+        {
+            sq0 = (1 + rotation_matrix[0, 0] + rotation_matrix[1, 1] + rotation_matrix[2, 2]) / 4;
+            sq1 = (1 + rotation_matrix[0, 0] - rotation_matrix[1, 1] - rotation_matrix[2, 2]) / 4;
+            sq2 = (1 - rotation_matrix[0, 0] + rotation_matrix[1, 1] - rotation_matrix[2, 2]) / 4;
+            sq3 = (1 - rotation_matrix[0, 0] - rotation_matrix[1, 1] + rotation_matrix[2, 2]) / 4;
+
+            ch0 = sq0.CompareTo((q0 * q0));
+            ch1 = sq1.CompareTo((q1 * q1));
+            ch2 = sq2.CompareTo((q2 * q2));
+            ch3 = sq3.CompareTo((q3 * q3));
+
+        }
+    }
+}
